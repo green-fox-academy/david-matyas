f56b46353d9015fcd49efba80548f05c5fbaf347
davidmatyas <david.matyas.cz@gmail.com>
2021-12-14 22:46:10 +0100

    Java Function partHardOne3

--- JavaW2-ArrayFunction/Palindrome.java
+++ JavaW2-ArrayFunction/Palindrome.java
@@ -1,27 +1,27 @@
 import java.lang.reflect.Array;
+import java.util.Scanner;
 
 public class Palindrome {
     public static void main(String[] args) {
-        String word = "Ahoj";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Word or few words");
+        String word = scanner.next();
         System.out.println(createPalindrome(word));
     }
 
     public static char[] createPalindrome(String word) {
-        char[] allChar = new char[word.length()];
+        //      char[] allChar = new char[word.length()];
         char[] reverseChar = new char[word.length() * 2];
-        int k;
-        k = (word.length()  - 1);
-        for (int i = 0; i < allChar.length; i++) {
-            allChar[i] = word.charAt(i);
+        int k = (word.length() - 1);
+        for (int i = 0; i < word.length(); i++) {
+            //      allChar[i] = word.charAt(i);
             reverseChar[i] = word.charAt(i);
         }
 
-        for (int j = allChar.length; j < reverseChar.length; j++) {
-            reverseChar[j] = allChar[k];
+        for (int j = word.length(); j < reverseChar.length; j++) {
+            reverseChar[j] = reverseChar[k];
             k--;
         }
-
-
         return reverseChar;
     }
 }
