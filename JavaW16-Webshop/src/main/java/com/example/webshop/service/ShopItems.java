92ba240dcc419719d8ecddb6bd70296c8e5a0e01
davidmatyas <david.matyas.cz@gmail.com>
2022-03-29 21:39:26 +0200

    add webshop table of product

--- JavaW16-Webshop/src/main/java/com/example/webshop/service/ShopItems.java
+++ JavaW16-Webshop/src/main/java/com/example/webshop/service/ShopItems.java
@@ -10,28 +10,28 @@ public class ShopItems {
                     "Running shoes",
                     "Nike running shoes for every night sport",
                     1000.0,
-                    5)
-            ),
-            new WebShopItem(
-                    "Printer",
-                    "Some HP printer that will sometimes print",
-                    3000.0,
-                    2),
-            new WebShopItem(
-                    "Coca cola",
-                    "0.5 vintage coke",
-                    25.0,
-                    0),
-            new WebShopItem(
-                    "Wokin",
-                    "Chicken with fried rice and WOKIN sauce",
-                    119.0,
-                    100),
-            new WebShopItem(
-                    "T-shirt",
-                    "Blue with a corn on bike",
-                    300.0,
-                    1)
+                    5),
+                    new WebShopItem(
+                            "Printer",
+                            "Some HP printer that will sometimes print",
+                            3000.0,
+                            2),
+                    new WebShopItem(
+                            "Coca cola",
+                            "0.5 vintage coke",
+                            25.0,
+                            0),
+                    new WebShopItem(
+                            "Wokin",
+                            "Chicken with fried rice and WOKIN sauce",
+                            119.0,
+                            100),
+                    new WebShopItem(
+                            "T-shirt",
+                            "Blue with a corn on bike",
+                            300.0,
+                            1)
+            )
     );
 
     public static List<WebShopItem> getItems() {
