90ff875c187ce14c473de8ad517e676e2ad2c51e
davidmatyas <david.matyas.cz@gmail.com>
2022-03-30 19:34:27 +0200

    add webshop add product detailm

--- JavaW16-Webshop/src/main/java/com/example/webshop/controller/WebshopItemController.java
+++ JavaW16-Webshop/src/main/java/com/example/webshop/controller/WebshopItemController.java
@@ -1,13 +1,39 @@
 package com.example.webshop.controller;
 
+import com.example.webshop.model.WebShopItem;
+import com.example.webshop.service.ShopItems;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 
 public class WebshopItemController {
     @GetMapping("/addItem")
     public String getItemForm() {
-    return "addItem";
+        return "addItem";
+    }
+
+    @GetMapping("/detail/{item}")
+    public String getDetail(Model model,
+                            @PathVariable("item") Integer itemId) {
+model.addAttribute("item",ShopItems.getItems().get(itemId));
+        return "detail";
+    }
+
+    @PostMapping("/addItem")
+    public String addItem(
+            @RequestParam String name,
+            @RequestParam String description,
+            @RequestParam Double price,
+            @RequestParam int onStock
+    ) {
+        WebShopItem item = new WebShopItem(
+                name,
+                description,
+                price,
+                onStock       );
+        ShopItems.getItems().add(item);
+        return "redirect:/";
     }
 }
